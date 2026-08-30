7d749e9cbc02cdfa882a0e4e84de3b90cf5c21a9
Christopher Altamirano <caltam600@gmail.com>
2026-08-30 09:48:06 -0400

    Fix navigation links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import myImage from '../assets/myImage.png'
 import project1Img from '../assets/safeserve.png'
 import project2Img from '../assets/tourapp.png'
-
+import { Link } from 'react-router-dom'
 
 
 function Home(){
@@ -25,7 +25,7 @@ function Home(){
             </div>
             <div className="qualities">
                 <div className="qualityBox glass">
-                    <a href="/Christopher Altamirano - Resume (IT).pdf"
+                    <a href={`${import.meta.env.BASE_URL}Christopher Altamirano - Resume (IT).pdf`}
                         target="_blank"
                         rel="noopener noreferrer"
                         className="viewbutton">
@@ -52,13 +52,13 @@ function Home(){
                         <img src={project2Img} className="projectImg"></img>
                         <h3 className="projectTitle">Tour App</h3>
                         <p className="projectDesc remMar">An android app that helps users find tours around them, and add to their booking. Also has the ability to create tour options.</p>
-                        <a href="/Projects/tourapp" class="projectLink delink">View Project</a>
+                        <Link to="/Projects/tourapp" className="projectLink delink">View Project</Link>
                     </div>
                     <div className="projectBox glass">
                         <img src={project1Img} className="projectImg"></img>
                         <h3 className="projectTitle">SafeServe</h3>
                         <p className="projectDesc remMar">A website showcasing a restaurant search tool with filters. Allows users to save prefences and offer feedback.</p>
-                        <a href="/Projects/safeserve" class="projectLink delink">View Project</a>                    </div>
+                        <Link to="/Projects/safeserve" className="projectLink delink">View Project</Link>                    </div>
                 </div>
             </div>
 
